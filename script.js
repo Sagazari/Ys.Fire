e5f0ea0645845d93b3706946c68fa1548bcbce6f
Sagazari <lanny051601@gmail.com>
2025-05-06 19:21:53 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('run').addEventListener('click', () => {
-    const code = document.getElementById('code').value;
-    const output = document.getElementById('output');
-    output.textContent = 'Running Ys.Fire script...\n\n' + code + '\n\nOutput simulation here.';
-});
